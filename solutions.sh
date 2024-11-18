92563e187f9a18093decec9f4fe3b64dc42ee96e
Soner Ünal <159646672+soneruen@users.noreply.github.com>
2024-11-18 09:22:04 +0100

    Delete solutions.sh

--- solutions.sh
+++
@@ -1,8 +0,0 @@
-# Ausgabe der ersten drei Zeilen der Datei
-head -n 3 "../shell-lessons/shell-lesson/exercise-data/2014-01_JA.tsv"
-
-# Gesamtanzahl der Zeilen in jeder *.tsv-Datei zählen
-wc -l "../shell-lessons/shell-lesson/exercise-data/"*.tsv
-
-# Datei mit der meisten Zeilen finden
-wc -l "../shell-lessons/shell-lesson/exercise-data/"*.tsv | sort -nr | head -n 1
